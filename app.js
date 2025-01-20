fdc079dbd6d4b8bbc953088d84e84b31b3a85ea9
Ryu292 <163379190+Ryu-292@users.noreply.github.com>
2025-01-20 19:49:41 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// Select the toggle button and menu
+const toggleButton = document.getElementById('mobile-menu');
+const menu = document.querySelector('.navbar__menu');
+
+// Add click event listener to the toggle button
+toggleButton.addEventListener('click', () => {
+    menu.classList.toggle('active'); // Toggle the 'active' class on the menu
+});
+
+window.addEventListener('load', () => {
+    const hiddenImage = document.querySelector('.hidden-image img');
+    hiddenImage.style.display = 'block'; // Ensure it's loaded but hidden
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Add the class to trigger the animation
+    document.body.classList.add("loaded");
+});
+
+
+
+
+
+
